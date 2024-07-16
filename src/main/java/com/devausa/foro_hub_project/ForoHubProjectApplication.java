05ef7ee0d010437b47836131e655bd76fc696eee
Cesar Ausa <cesarausaprog@gmail.com>
2024-07-16 11:59:11 -0300

    subiendo archivos

---
+++ src/main/java/com/devausa/foro_hub_project/ForoHubProjectApplication.java
@@ -0,0 +1,13 @@
+package com.devausa.foro_hub_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoHubProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devausa/foro_hub_project/ForoHubProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devausa.foro_hub_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
